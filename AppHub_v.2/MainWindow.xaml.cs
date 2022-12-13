b5d6c507460fbe2f97b18faf49a44bcb096db0c9
Jacob Ørnskov Andersen <jaco8678@elev.techcollege.dk>
2022-12-13 10:57:52 +0100

    Add project files.

---
+++ AppHub_v.2/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppHub_v._2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //lineTest.X2 = (stPage.Width - catLabel.Width);
+            lineTest.X2 = stPage.Width-25;
+        }
+
+        private void TopbarDrag_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void btnTopBarMenuClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnTopBarMenuMini_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        bool isRotated = false;
+        private void test_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (!isRotated) RotateBtn(btn, -90);
+            else RotateBtn(btn, 0);
+        }
+
+        public void RotateBtn(Button btn, int angle)
+        {
+            isRotated = !isRotated;
+            btn.RenderTransform = new RotateTransform(angle, 12.5, 12.5);
+        }
+    }
+}
---
+++ AppHub_v.2/UserControls/AppShortcut.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppHub_v._2.UserControls
+{
+    /// <summary>
+    /// Interaction logic for AppShortcut.xaml
+    /// </summary>
+    public partial class AppShortcut : UserControl
+    {
+        public AppShortcut()
+        {
+            InitializeComponent();
+        }
+
+        private void test_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Test");
+        }
+    }
+}
